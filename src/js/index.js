2af2ad5c10cd60f29db942930e10ae688ecfa896
fedmike <molnek87@mail.ru>
2025-10-29 21:25:14 +0900

    start new project

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+//import mobileNav from './modules/mobile-nav.js';
+//mobileNav();
+
+// import isAvifWebp from 'avif-webp-checker';
+// isAvifWebp({ mode: 'webp' });
+
+import tagline from './modules/tagline.js';
+tagline();
