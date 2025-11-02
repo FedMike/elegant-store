0c69e5c720ba1d44124955f0ff3aafd0ff21dd99
fedmike <molnek87@mail.ru>
2025-11-02 20:11:25 +0900

    swiper added

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 5,
+    spaceBetween: 24
+})
+
 //import mobileNav from './modules/mobile-nav.js';
 //mobileNav();
 
