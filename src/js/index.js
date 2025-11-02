eb7b50c64b70196352ba396b81bd289d0bf11f3d
fedmike <molnek87@mail.ru>
2025-11-02 23:04:38 +0900

    arrivals done

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
 import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
+//import 'swiper/css/bundle';
 
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 5,
-    spaceBetween: 24
+    slidesPerView: "auto",
+    spaceBetween: 24,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: 'true'
+    }
 })
 
 //import mobileNav from './modules/mobile-nav.js';
