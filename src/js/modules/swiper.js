99e03a781de037dda33925c338fc3cbd5838b9dc
fedmike <molnek87@mail.ru>
2025-11-05 17:26:23 +0900

    section arrivals mobile version done

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -3,11 +3,17 @@ import Swiper from 'swiper/bundle';
 function swiper() {
     const swiper = new Swiper('.swiper', {
         slidesPerView: "auto",
-        spaceBetween: 24,
+        spaceBetween: 12,
         
         pagination: {
             el: '.swiper-pagination',
             clickable: 'true'
+        },
+
+        breakpoints: {
+            820: {
+                spaceBetween: 24
+            }
         }
     })
 }
